c94acaaaf41806e2ebf1e21e54880c7443cba907
Jeff Hall <jeff.leon.hall@gmail.com>
2021-10-05 14:57:12 -0700

    wrote the checkBalance method for the createAccount function

--- create-account.js
+++ create-account.js
@@ -1,5 +1,16 @@
 function createAccount(pin, amount) {
-
+  let balance = amount;
+  let correctPin = pin;
+  return {
+    checkBalance(enteredPin) {
+      if (enteredPin !== correctPin) {
+        return 'Invalid PIN.'
+      }
+      else {
+        return balance;
+      }
+    }
+  }
 }
 
 module.exports = { createAccount };
