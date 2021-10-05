92ab291ff3d928a013595c357b2710875f732687
Jeff Hall <jeff.leon.hall@gmail.com>
2021-10-05 14:32:03 -0700

    implemented the guessingGame function

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,26 @@
 function guessingGame() {
+  let target = Math.floor(Math.random() * 100);
+  let hasWon = false;
+  let count = 0;
 
+  return function(guess) {
+    if (hasWon) {
+      return "The game is over, you already won!";
+    }
+    else if (guess < target) {
+      count++;
+      return `${guess} is too low!`;
+    }
+    else if (guess > target) {
+      count++;
+      return `${guess} is too high!`;
+    }
+    else {
+      count++;
+      hasWon = true;
+      return `You win! You found ${target} in ${count} guesses.`
+    }
+  }
 }
 
 module.exports = { guessingGame };
