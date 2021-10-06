47bc5fb45af8136924d01e12e6be22a90efcabea
Jeff Hall <jeff.leon.hall@gmail.com>
2021-10-05 17:08:56 -0700

    implemented the curriedAdd function

--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,24 @@
 function curriedAdd(total) {
-
-}
+  let sum = total || 0;
+  if (!sum) {
+    return sum;
+  }
+  else {
+    let someFunc = function(x) {
+      if (x === undefined) {
+        return sum;
+      }
+      else {
+        sum += x;
+        return someFunc;
+      }
+    }
+    return someFunc;
+  };
+};
 
 module.exports = { curriedAdd };
+
+// let firstAdder = curriedAdd(10)(7)
+
+// firstAdder(5)
